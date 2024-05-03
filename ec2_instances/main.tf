d1d29edfa8adc66fe496e7baf4d0d198a7342269
Thoughtbloom677 <nigel.stokes555@gmail.com>
2024-05-03 17:23:37 -0500

    adding ec2 module

---
+++ ec2_instances/main.tf
@@ -0,0 +1,17 @@
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "4.3.0"
+
+  count = 2
+  name  = "my-ec2-cluster-${count.index}"
+
+  ami                    = "ami-0c5204531f799e0c6"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
